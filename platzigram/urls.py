f38bccda37579e06ff6116e58cac1cc91a44419b
Felipe Acosta <facosta0787@gmail.com>
2019-06-06 01:59:50 -0500

    Serving media files from my localhost on development

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+
 from posts import views as posts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', posts_views.list_posts)
-]
+    path('posts/', posts_views.list_posts),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
