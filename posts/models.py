21765ed0fb385bf7b39061bb42d6bdbbb3214de1
Felipe Acosta <facosta0787@gmail.com>
2019-06-06 00:36:07 -0500

    Basics of django

---
+++ posts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_admin = models.BooleanField(default=False)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.email
